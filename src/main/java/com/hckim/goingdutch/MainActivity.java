d45e3a24fd3e0e63b2bad65d616b0711f69ae3b2
Hongchul Kim <gsjune@naver.com>
2017-08-22 09:10:51 +0900

    1/N 앱 모듈로 생성, 레이아웃 구조 잡기

--- src/main/java/com/hckim/goingdutch/MainActivity.java
+++ src/main/java/com/hckim/goingdutch/MainActivity.java
@@ -1,13 +1,27 @@
 package com.hckim.goingdutch;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView mPersonalMoneyTextView;
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPersonalMoneyTextView = (TextView) findViewById(R.id.money_text);
+
+    }
+
+    public void personalButtonClicked(View view) {
+//        Toast.makeText(this, "잘 눌림", Toast.LENGTH_SHORT).show();
+        mPersonalMoneyTextView.setText("10");
+
     }
 }
